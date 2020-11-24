3ab80b9fadd9b406784ade3d0f82981bddea25a1
Mihail-sudo <57751986+Mihail-sudo@users.noreply.github.com>
2020-11-24 23:10:08 +0500

    Add files via upload

---
+++ create_password.sh
@@ -0,0 +1,19 @@
+SYMBOLS=""
+for symbol in {A..Z} {a..z} {0..9}; do SYMBOLS=$SYMBOLS$symbol; done
+PWD_LENGTH=16  
+PASSWORD=""    
+RANDOM=256   
+
+while read line
+do
+	PASSWORD=""  
+	for i in `seq 1 $PWD_LENGTH`
+		do 
+		PASSWORD=$PASSWORD${SYMBOLS:$(expr $RANDOM % ${#SYMBOLS}):1}
+		done
+	echo $PASSWORD
+	htpasswd -b /usr/local/etc/nginx/.htpasswd $line $PASSWORD
+	##curl -u $line:$PASSWORD http://localhost:8080
+	curl -X POST "$line:$PASSWORD" http://localhost:8080
+
+done < "${1:-dev/stdin}"
