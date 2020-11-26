df96cb48e453798d14dc1872980ff66d22d3ce8d
Mihail-sudo <57751986+Mihail-sudo@users.noreply.github.com>
2020-11-26 13:08:45 +0500

    Update create_password.sh

--- create_password.sh
+++ create_password.sh
@@ -1,17 +1,17 @@
 SYMBOLS=""
 for symbol in {A..Z} {a..z} {0..9}; do SYMBOLS=$SYMBOLS$symbol; done
-PWD_LENGTH=16  
+PASSWORD_LENGTH=16  
 PASSWORD=""    
 RANDOM=256   
 
-while read line; do
-	PASSWORD=""  
-	for i in `seq 1 $PWD_LENGTH`; do
+while read login; do
+	PASSWORD=""
+	for i in `seq 1 $PASSWORD_LENGTH`; do
 		PASSWORD=$PASSWORD${SYMBOLS:$(expr $RANDOM % ${#SYMBOLS}):1}
 	done
 	echo $PASSWORD
-	htpasswd -b $2 $line $PASSWORD
+	htpasswd -b $2 $login $PASSWORD
 	##curl -u $line:$PASSWORD http://localhost:8080
-	curl -X POST "$line:$PASSWORD" http://localhost:8080
+	curl -X POST "$login:$PASSWORD" http://localhost:8080
 
 done < "${1:-dev/stdin}"
